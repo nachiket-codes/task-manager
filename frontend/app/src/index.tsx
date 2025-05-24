fa36a4402f06f8a706bd16252f5031345608a94f
nachiket-codes <fitnessnachiket@gmail.com>
2025-05-25 00:35:16 +0530

    Add Register page

--- frontend/app/src/index.tsx
+++ frontend/app/src/index.tsx
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Register from './components/Register';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/register" element={<Register/>}/>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
