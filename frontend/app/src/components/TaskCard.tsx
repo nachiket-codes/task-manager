8405ee94fb81fe34cbefc3cbb284105ede230687
nachiket-codes <fitnessnachiket@gmail.com>
2025-05-25 21:30:47 +0530

    Add list of tasks frontend

---
+++ frontend/app/src/components/TaskCard.tsx
@@ -0,0 +1,13 @@
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const TaskCard: React.FC = () => {
+    return (
+        <div className="p-3 flex border-2 border-black text-2xl w-[full] bg-black text-white font-semibold hover:bg-white hover:text-black">
+            <h1 className="flex-1 cursor-pointer">The task it thos</h1>
+            <FontAwesomeIcon className="p-2 hover:text-red-600 ease-out duration-200 cursor-pointer" icon={faTrash}/>
+        </div>
+    )
+}
+
+export default TaskCard;
