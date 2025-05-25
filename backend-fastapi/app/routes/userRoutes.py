aa91ab11ae7c7b8a4e0d3461f60ac35291140ac8
nachiket-codes <fitnessnachiket@gmail.com>
2025-05-25 08:42:26 +0530

    Add redux and complete working of register functionality

--- backend-fastapi/app/routes/userRoutes.py
+++ backend-fastapi/app/routes/userRoutes.py
@@ -4,10 +4,12 @@ from database import usersCollection
 from authentication import createAccessToken
 from hashing import Hash
 
+
 router = APIRouter(prefix = '/auth', tags = ["Authentication"])
 
 @router.post('/register')
 async def register(userData: Register):
+    print('here')
     user = await usersCollection.find_one({'email' : userData.email})
     if user:
         raise HTTPException(
@@ -18,15 +20,20 @@ async def register(userData: Register):
     newUserDoc = {
         "username" : userData.username,
         "email" : userData.email,
-        "password" : Hash().hash(userData.password)
+        "password" : Hash.hash(userData.password)
     }
     
-    addedUserId = await usersCollection.insert_one(newUserDoc)
-    if not addedUserId:
+    result = await usersCollection.insert_one(newUserDoc)
+    if not result.inserted_id:
         raise HTTPException(
             status_code = status.HTTP_400_BAD_REQUEST,
             detail = {"message" : "Failed to add user"}
         )
     
     tokenData = createAccessToken(data = {'sub' : userData.email})
-    return tokenData
+    return {
+        'id' : str(result.inserted_id),
+        'username': userData.username,
+        'email': userData.email,
+        'token' : tokenData.access_token
+    }
